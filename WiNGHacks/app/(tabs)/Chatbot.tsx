c8640044694030462b4f84a2cec1b7489328e751
anyaasutaria <ananyaa.sutaria@gmail.com>
2025-02-07 23:10:39 -0500

    colors

--- WiNGHacks/app/(tabs)/Chatbot.tsx
+++ WiNGHacks/app/(tabs)/Chatbot.tsx
@@ -10,7 +10,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '##ffcc99', dark: '#ffe6cc' }}
+      headerBackgroundColor={{ light: '##ffcc99', dark: '#ffcc99' }}
       headerImage={
         <IconSymbol
           size={310}
