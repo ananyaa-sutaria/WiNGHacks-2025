7bae9a3f31389a3584ff201799f7f03ea996c795
alyssagriesman <agriesman@icloud.com>
2025-02-07 23:58:44 -0500

    pic

--- WiNGHacks/app/(tabs)/Calendar.tsx
+++ WiNGHacks/app/(tabs)/Calendar.tsx
@@ -10,12 +10,10 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '#CBC3E3', dark: '#FF8C00' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+        <Image
+          source={require('@/assets/images/calendarpic.png')}  // Replace with your image path
           style={styles.headerImage}
         />
       }>
