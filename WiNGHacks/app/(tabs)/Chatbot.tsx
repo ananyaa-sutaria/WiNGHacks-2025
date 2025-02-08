f3f82dd5736b66e34041a012c5b82155de79fc95
anyaasutaria <ananyaa.sutaria@gmail.com>
2025-02-07 23:08:57 -0500

    testing colors

--- WiNGHacks/app/(tabs)/Chatbot.tsx
+++ WiNGHacks/app/(tabs)/Chatbot.tsx
@@ -10,7 +10,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '##ffcc99', dark: '#ffa64d' }}
       headerImage={
         <IconSymbol
           size={310}
